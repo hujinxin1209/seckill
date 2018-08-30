0a0f8906dbd38c242fb6fbd55019855f2a3b90a0
hujinxin1209 <565831772@qq.com>
2018-08-30 22:27:36 +0800

    cookie token

--- src/main/java/com/ugia/seckill/controller/GoodsController.java
+++ src/main/java/com/ugia/seckill/controller/GoodsController.java
@@ -6,10 +6,15 @@ import javax.validation.Valid;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.druid.util.StringUtils;
+import com.ugia.seckill.domain.MiaoshaUser;
+import com.ugia.seckill.domain.User;
 import com.ugia.seckill.redis.RedisService;
 import com.ugia.seckill.result.CodeMsg;
 import com.ugia.seckill.result.Result;
@@ -18,19 +23,29 @@ import com.ugia.seckill.service.UserService;
 import com.ugia.seckill.util.ValidatorUtil;
 import com.ugia.seckill.vo.LoginVo;
 
+import javassist.expr.NewArray;
+
 @Controller
-@RequestMapping("/login")
-public class LoginController {
-	private static org.slf4j.Logger log = LoggerFactory.getLogger(LoginController.class);
+@RequestMapping("/goods")
+public class GoodsController {
+	private static org.slf4j.Logger log = LoggerFactory.getLogger(GoodsController.class);
 	
 	@Autowired
 	RedisService redisService;
 	@Autowired
 	MiaoshaUserService miaoshaUserService;
 	
-	@RequestMapping("/to_login")
-	public String toLogin(){
-		return "login";
+	@RequestMapping("/to_list")
+	public String toLogin(HttpServletResponse response, Model model,
+			@CookieValue(value=MiaoshaUserService.COOKIE_NAME_TOKEN, required=false)String cookieToken,
+			@RequestParam(value=MiaoshaUserService.COOKIE_NAME_TOKEN,required=false)String paramToken){
+		if(StringUtils.isEmpty(cookieToken) && StringUtils.isEmpty(paramToken)) {
+			return "login";
+		}
+		String token = StringUtils.isEmpty(paramToken)?cookieToken : paramToken;
+		MiaoshaUser user = miaoshaUserService.getByToken(response, token);
+		model.addAttribute("user", user);
+		return "goods_list";
 	}
 	
 	@RequestMapping("/do_login")
--- src/main/java/com/ugia/seckill/service/MiaoshaUserService.java
+++ src/main/java/com/ugia/seckill/service/MiaoshaUserService.java
@@ -1,45 +1,80 @@
 package com.ugia.seckill.service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.druid.util.StringUtils;
 import com.ugia.seckill.dao.MiaoshaUserDao;
 import com.ugia.seckill.domain.MiaoshaUser;
 import com.ugia.seckill.exception.GlobalException;
+import com.ugia.seckill.redis.MiaoshaUserKey;
+import com.ugia.seckill.redis.RedisService;
 import com.ugia.seckill.result.CodeMsg;
 import com.ugia.seckill.util.MD5Util;
+import com.ugia.seckill.util.UUIDUtil;
 import com.ugia.seckill.vo.LoginVo;
 
 @Service
 public class MiaoshaUserService {
-	
+	public static final String COOKIE_NAME_TOKEN = "token";
+
 	@Autowired
 	MiaoshaUserDao miaoshaUserDao;
-	
+
+	@Autowired
+	RedisService redisService;
+
 	public MiaoshaUser getById(long id) {
 		return miaoshaUserDao.getById(id);
 	}
 
-	public boolean login(LoginVo loginVo) {
-		if(loginVo == null) {
+	public boolean login(HttpServletResponse response, LoginVo loginVo) {
+		if (loginVo == null) {
 			throw new GlobalException(CodeMsg.SERVER_ERROR);
 		}
-		
+
 		String mobile = loginVo.getMobile();
 		String formPassword = loginVo.getPassword();
-		
+
 		MiaoshaUser user = getById(Long.parseLong(mobile));
-		if(user == null) {
+		if (user == null) {
 			throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
 		}
-		
+
 		// 验证密码
 		String dbPassword = user.getPassword();
 		String saltDB = user.getSalt();
 		String calcPas = MD5Util.formPassToDBPass(formPassword, saltDB);
-		if(!calcPas.equals(dbPassword)) {
-			throw new GlobalException( CodeMsg.PASSWORD_WRONG);
+		if (!calcPas.equals(dbPassword)) {
+			throw new GlobalException(CodeMsg.PASSWORD_WRONG);
 		}
+		// 生成cookie
+		addCookie(response, user);
 		return true;
+	}
+
+	private void addCookie(HttpServletResponse response, MiaoshaUser user) {
+		
+		String token = UUIDUtil.uuid();
+		redisService.set(MiaoshaUserKey.token, token, user);
+		Cookie cookie = new Cookie(COOKIE_NAME_TOKEN, token);
+		cookie.setMaxAge(MiaoshaUserKey.token.expireSeconds());
+		cookie.setPath("/");
+		response.addCookie(cookie);
+	}
+
+	public MiaoshaUser getByToken(HttpServletResponse response, String token) {
+		if (StringUtils.isEmpty(token)) {
+			return null;
+		}
+		MiaoshaUser user = redisService.get(MiaoshaUserKey.token, token, MiaoshaUser.class);
+		// 延长token有效期
+		if(user != null) {
+			addCookie(response, user);
+		}
+		return user;
 	}
 }
